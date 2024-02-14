33915a8e8f7a31d4903c626b05d7a021ebb58514
[tariqbaater] <[tariqbaater@gmail.com]>
2024-02-14 19:44:51 +0300

    modified Astro updates to stable

--- mappings.lua
+++ mappings.lua
@@ -20,21 +20,24 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- change next buffer with tab
+    ["<tab>"] = { ":bnext<CR>", desc = "Next buffer" },
+    -- change previous buffer with shift + tab
+    ["<s-tab>"] = { ":bprevious<CR>", desc = "Previous buffer" },
     -- run python file
     ["<leader>r"] = { ":w<CR>:sp |terminal python3 %<CR>i", desc = "Run Python File" },
-        -- Run JS File
+    -- Run JS File
     ["<leader>j"] = { ":w<CR>:sp |terminal node %<CR>i", desc = "Run JS File" },
-
-    ["<leader>i"] = { ":w<CR>:sp | terminal Rscript %<CR>", desc = "Run R File" },
+    -- Compile typescript File
+    ["<leader>k"] = { ":w<CR>:sp |terminal tsc <CR>i", desc = "Compile TS File" },
+    -- Run R File
+    ["<leader>i"] = { ":w<CR>:sp | terminal Rscript %<CR>i", desc = "Run R File" },
     -- for saving
     ["<leader>s"] = { ":w<CR>", desc = "Save" },
     -- for formatting sql
     ["<leader>q"] = { ":%!sqlformat --reindent --keywords upper --identifiers lower - <CR>", desc = "Format SQL" },
     -- for deploying DBUI
     ["<leader>d"] = { ":DBUIToggle<CR>", desc = "Deploy DBUI" },
-
   },
   t = {
     -- setting a mapping to false will disable it
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,5 +1,6 @@
 return {
   -- You can also add new plugins here as well:
+  -- Add LazyVim and all your plugins
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
   -- {
@@ -13,8 +14,8 @@ return {
     "deepskyblue86/bard-nvim",
     lazy = false,
     config = function()
-      require('bard').setup({bardcli_path="/opt/homebrew/bin/bard-cli", bardcli_config_path="$HOME/.bardcli.yaml"})
-    end
+      require("bard").setup { bardcli_path = "/opt/homebrew/bin/bard-cli", bardcli_config_path = "$HOME/.bardcli.yaml" }
+    end,
   },
   {
     "diepm/vim-rest-console",
@@ -25,7 +26,9 @@ return {
   },
   {
     "sourcegraph/sg.nvim",
-    dependencies = { "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]] },
+    dependencies = {
+      "nvim-lua/plenary.nvim", --[[ "nvim-telescope/telescope.nvim ]]
+    },
     -- If you have a recent version of lazy.nvim, you don't need to add this!
     build = "nvim -l build/init.lua",
     lazy = false,
@@ -37,27 +40,26 @@ return {
   {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
-    { 'tpope/vim-dadbod', lazy = true },
-    -- { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
-  },
-  cmd = {
-    'DBUI',
-    'DBUIToggle',
-    'DBUIAddConnection',
-    'DBUIFindBuffer',
-    'DBRowsCount',
-  },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-    vim.g.db_ui_show_row_count = 1
-    vim.g.db_ui_row_count_format = ' '
-  end,
+      { "tpope/vim-dadbod", lazy = true },
+      -- { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+      "DBRowsCount",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.db_ui_show_row_count = 1
+      vim.g.db_ui_row_count_format = " "
+    end,
   },
   {
     "kristijanhusak/vim-dadbod-completion",
     lazy = false,
-
   },
   {
     "Exafunction/codeium.vim",
@@ -75,6 +77,7 @@ return {
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     event = "VeryLazy",
     config = function()
+      ---@diagnostic disable-next-line: missing-fields
       require("nvim-surround").setup {
         -- Configuration here, or leave empty to use defaults
       }
@@ -160,5 +163,73 @@ return {
         "<cmd>:VenvSelectCached<cr>",
       },
     },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    opts = {
+      -- configuration options...
+    },
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+      cmdline = {
+        view = "cmdline_popup",
+      },
+      messages = {
+        enabled = true,
+        view = "mini",
+      },
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+        },
+      },
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    },
+  },
+  {
+    "easymotion/vim-easymotion",
+    lazy = false,
+  },
+  {
+    "lervag/vimtex",
+    dependencies = {
+      "micangl/cmp-vimtex",
+    },
+    version = "*",
+    -- event = { "BufReadPre", "BufNewFile" },       -- WARNING: adding events can prevent synctex inverse search from working
+    config = function()
+      -- vim.g['vimtex_view_method'] = 'zathura'     -- main variant with xdotool (requires X11; not compatible with wayland)
+      vim.g["vimtex_view_method"] = "zathura_simple" -- for variant without xdotool to avoid errors in wayland
+      vim.g["vimtex_quickfix_mode"] = 0 -- suppress error reporting on save and build
+      vim.g["vimtex_mappings_enabled"] = 0 -- Ignore mappings
+      vim.g["vimtex_indent_enabled"] = 0 -- Auto Indent
+      vim.g["tex_flavor"] = "latex" -- how to read tex files
+      vim.g["tex_indent_items"] = 0 -- turn off enumerate indent
+      vim.g["tex_indent_brace"] = 0 -- turn off brace indent
+      vim.g["vimtex_syntax_enabled"] = 1 -- Syntax highlighting
+      vim.g["vimtex_context_pdf_viewer"] = "okular" -- external PDF viewer run from vimtex menu command
+      vim.g["vimtex_log_ignore"] = { -- Error suppression:
+        "Underfull",
+        "Overfull",
+        "specifier changed to",
+        "Token not allowed in a PDF string",
+      }
+    end,
   },
 }
