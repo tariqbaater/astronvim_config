375825f8ba44ab24cdf09546ef5735815978dc74
[tariqbaater] <[tariqbaater@gmail.com]>
2024-02-26 21:13:07 +0300

    updated keymaps with map silent

--- mappings.lua
+++ mappings.lua
@@ -3,6 +3,7 @@
 -- Please use this mappings table to set keyboard mapping since this is the
 -- lower level configuration and more robust one. (which-key will
 -- automatically pick-up stored data by this setting.)
+local map = vim.api.nvim_set_keymap
 return {
   -- first key is the mode
   n = {
@@ -19,25 +20,49 @@ return {
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
     -- change next buffer with tab
-    ["<tab>"] = { ":bnext<CR>", desc = "Next buffer" },
+    map("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true, desc = "Next buffer" }),
     -- change previous buffer with shift + tab
-    ["<s-tab>"] = { ":bprevious<CR>", desc = "Previous buffer" },
+    map("n", "<S-Tab>", ":bprevious<CR>", { noremap = true, silent = true, desc = "Previous buffer" }),
+
+    ["<leader>b"] = { name = "Buffers" },
     -- run python file
-    ["<leader>r"] = { ":w<CR>:sp |terminal python3 %<CR>i", desc = "Run Python File" },
+    -- ["<leader>r"] = { ":w<CR>:sp |terminal python3 %<CR>i", desc = "Run Python File" },
+    map(
+      "n",
+      "<leader>r",
+      ":w<CR>:sp |terminal python3 %<CR>i",
+      { noremap = true, silent = true, desc = "Run Python File" }
+    ),
     -- Run JS File
-    ["<leader>j"] = { ":w<CR>:sp |terminal node %<CR>i", desc = "Run JS File" },
+    map("n", "<leader>j", ":w<CR>:sp |terminal node %<CR>i", { noremap = true, silent = true, desc = "Run JS File" }),
+    -- Compile c++
+    map(
+      "n",
+      "<leader>D",
+      ":w<CR>:sp |terminal g++ % -o %:r && ./%:r<CR>i",
+      { noremap = true, silent = true, desc = "Compile C++ File" }
+    ),
     -- Compile typescript File
-    ["<leader>k"] = { ":w<CR>:sp |terminal tsc <CR>i", desc = "Compile TS File" },
+    map("n", "<leader>k", ":w<CR>:sp |terminal tsc <CR>i", { noremap = true, silent = true, desc = "Compile TS File" }),
     -- Run R File
-    ["<leader>i"] = { ":w<CR>:sp | terminal Rscript %<CR>i", desc = "Run R File" },
+    map(
+      "n",
+      "<leader>i",
+      ":w<CR>:sp | terminal Rscript %<CR>i",
+      { noremap = true, silent = true, desc = "Run R File" }
+    ),
     -- for saving
-    ["<leader>s"] = { ":w<CR>", desc = "Save" },
+    map("n", "<leader>s", ":w<CR>", { noremap = true, silent = true, desc = "Save File" }),
     -- for formatting sql
-    ["<leader>q"] = { ":%!sqlformat --reindent --keywords upper --identifiers lower - <CR>", desc = "Format SQL" },
+    map(
+      "n",
+      "<leader>q",
+      ":%!sqlformat --reindent --keywords upper --identifiers lower - <CR>",
+      { noremap = true, silent = true, desc = "Format SQL" }
+    ),
     -- for deploying DBUI
-    ["<leader>d"] = { ":DBUIToggle<CR>", desc = "Deploy DBUI" },
+    map("n", "<leader>W", ":DBUIToggle<CR>", { noremap = true, silent = true, desc = "Deploy DBUI" }),
     -- -- binding for macro
     -- ["<leader>T"] = { "^f,<t_˝a>a<BS><CR>jk@q", desc = "Unlisting Macro" },
     -- ["<leader>L"] = { "A,<Space>jkJ@W", desc = "Listing Macro" },
--- plugins/user.lua
+++ plugins/user.lua
@@ -138,24 +138,31 @@ return {
   {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-      -- add any options here
-      cmdline = {
-        view = "cmdline_popup",
-      },
-      messages = {
-        enabled = true,
-        view = "mini",
-      },
-      lsp = {
-        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+    config = function()
+      require("noice").setup {
+        cmdline = {
+          view = "cmdline_popup",
         },
-      },
-    },
+        messages = {
+          enabled = true,
+          view = "mini",
+        },
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+          hover = {
+            enabled = false,
+          },
+          signature = {
+            enabled = false,
+          },
+        },
+      }
+    end,
     dependencies = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
       "MunifTanjim/nui.nvim",
